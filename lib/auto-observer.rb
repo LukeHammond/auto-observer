efd7891b63163cb88a70db4a537b4e2caa5a1ee7
LukeHammond <luke.e.j.hammond@gmail.com>
2012-02-21 19:08:20 -0500

    simplify usage by adding includes to AutoObserver module

--- lib/auto-observer.rb
+++ lib/auto-observer.rb
@@ -1,6 +1,15 @@
 require "auto-observer/version"
 
 module AutoObserver
+  extend ActiveSupport::Concern
+  included do
+
+    class_eval(<<-EOS, __FILE__, __LINE__ + 1)
+      extend ActiveModel::Callbacks
+      include AutoObserver::Callbacks if defined?(AutoObserver::Callbacks)
+      include AutoObserver::Observing if defined?(AutoObserver::Observing)
+    EOS
+  end
 end
 require 'auto-observer/observing'
 require 'auto-observer/callbacks'
