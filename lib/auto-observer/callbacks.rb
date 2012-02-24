e98d9e06f205dc7be8f65156e9cdc83ee41cfb85
LukeHammond <luke.e.j.hammond@gmail.com>
2012-02-24 09:41:23 -0500

    convert class_eval to define_method and send were needed

--- lib/auto-observer/callbacks.rb
+++ lib/auto-observer/callbacks.rb
@@ -5,14 +5,20 @@ module AutoObserver
 
     included do
       @callbacks.each do |method|
-        class_eval(<<-EOS, __FILE__, __LINE__ + 1)
-          def #{method}_with_run_callbacks(*args, &block)
-            result = nil
-            run_callbacks(:#{method}) do
-              #{method}_without_run_callbacks(*args, &block)
-            end
+        define_method("#{method}_with_run_callbacks") do |*args, &block|
+          run_callbacks("#{method}") do
+            send("#{method}_without_run_callbacks", *args, &block)
           end
-        EOS
+        end
+        
+        # class_eval(<<-EOS, __FILE__, __LINE__ + 1)
+        #   def #{method}_with_run_callbacks(*args, &block)
+        #     result = nil
+        #     run_callbacks(:#{method}) do
+        #       #{method}_without_run_callbacks(*args, &block)
+        #     end
+        #   end
+        # EOS
         alias_method_chain(method, :run_callbacks)
       end
     end
--- lib/auto-observer/observing.rb
+++ lib/auto-observer/observing.rb
@@ -13,15 +13,22 @@ module AutoObserver
         #   end
         #   result
         # end
-        class_eval(<<-EOS, __FILE__, __LINE__ + 1)
-          def #{method}_with_notifications(*args, &block)
-            notify_observers(:before_#{method})
-            if result = #{method}_without_notifications(*args, &block)
-              notify_observers(:after_#{method})
-            end
-            result
+        define_method("#{method}_with_notifications") do |*args, &block|
+          notify_observers("before_#{method}")
+          if result = send("#{method}_without_notifications", *args, &block)
+            notify_observers("after_#{method}")
           end
-        EOS
+        end
+        
+        # class_eval(<<-EOS, __FILE__, __LINE__ + 1)
+        #   def #{method}_with_notifications(*args, &block)
+        #     notify_observers(:before_#{method})
+        #     if result = #{method}_without_notifications(*args, &block)
+        #       notify_observers(:after_#{method})
+        #     end
+        #     result
+        #   end
+        # EOS
         alias_method_chain(method, :notifications)
       end
     end
